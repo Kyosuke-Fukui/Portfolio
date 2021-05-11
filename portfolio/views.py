f6a1c7caebe96f81cc2d04ccdd5f73244ff1310c
Kyosuke-Fukui <fs72os.kyosuke@gmail.com>
2021-05-12 01:11:34 +0900

    first commit

---
+++ portfolio/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from .models import Profile, Skill, Work
+
+def index(request):
+    profile = Profile.objects.all().last()
+    skills = Skill.objects.all()
+    works = Work.objects.all().order_by('-published')[:3]
+
+
+    context = {
+        'profile':profile,
+        'skills':skills,
+        'works':works,
+    }
+    return render(request, 'index.html', context)
